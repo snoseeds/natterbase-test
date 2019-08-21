92202a9606ff9a82851935642f8cd9e1680a1b43
Nurudeen Soremekun <olabisi@codesquad.co>
2019-08-21 12:26:10 +0100

    ft(api): Add lowestCircularIndex api endpoint
    [finishes #question3]

--- server/__tests__/3-questionThreeSolutions.test.js
+++ server/__tests__/3-questionThreeSolutions.test.js
@@ -8,26 +8,44 @@ chai.use(chaiHttp);
 
 describe('Testing Question 3 Solution Controller', () => {
   describe('Testing Calculate Lowest Possible Index for Aladdin\'s Circular Trip', () => {
-    const magicForFeasibleTrip = [3, 2, 5, 4];
-    const distForFeasibleTrip = [2, 3, 4, 2];
-    const distForInfeasibleTrip = [2, 3, 4, 7];
     const lowestCircularTripIndexUrl = '/api/v1/question-three-solutions/lowest-index-for-circular-trips';
     it(
-      'should return lowest ciruclar index when the circular trip is feasible',
+      'should return lowest ciruclar index of "0" when the circular trip is feasible at "0"',
       (done) => {
         chai.request(app)
           .post(lowestCircularTripIndexUrl)
           .send({
-            magic: magicForFeasibleTrip,
-            dist: distForFeasibleTrip,
+            magic: [3, 2, 5, 4],
+            dist: [2, 3, 4, 2],
           })
           .end((error, response) => {
             expect(response.body).to.be.an('object');
             expect(response).to.have.status(200);
             expect(response.body.status).to.equal(200);
             expect(response.body).to.have.property('lowestIndex');
-            expect(Number.isInteger(response.body.lowestIndex)).to.equal('true');
+            expect(Number.isInteger(response.body.lowestIndex)).to.equal(true);
             expect(response.body.lowestIndex).to.equal(0);
+            done();
+          });
+      },
+    );
+
+    it(
+      'should return lowest ciruclar index of "1" when the circular trip is first feasible at "1"',
+      (done) => {
+        chai.request(app)
+          .post(lowestCircularTripIndexUrl)
+          .send({
+            magic: [1, 3, 4, 4, 1],
+            dist: [1, 4, 2, 2, 2],
+          })
+          .end((error, response) => {
+            expect(response.body).to.be.an('object');
+            expect(response).to.have.status(200);
+            expect(response.body.status).to.equal(200);
+            expect(response.body).to.have.property('lowestIndex');
+            expect(Number.isInteger(response.body.lowestIndex)).to.equal(true);
+            expect(response.body.lowestIndex).to.equal(1);
             done();
           });
       },
@@ -39,15 +57,15 @@ describe('Testing Question 3 Solution Controller', () => {
         chai.request(app)
           .post(lowestCircularTripIndexUrl)
           .send({
-            magic: magicForFeasibleTrip,
-            dist: distForInfeasibleTrip,
+            magic: [3, 2, 5, 4],
+            dist: [2, 3, 4, 7],
           })
           .end((error, response) => {
             expect(response.body).to.be.an('object');
-            expect(response).to.have.status(200);
-            expect(response.body.status).to.equal(200);
+            expect(response).to.have.status(400);
+            expect(response.body.status).to.equal(400);
             expect(response.body).to.have.property('error');
-            expect(Number.isInteger(response.body.error)).to.equal('true');
+            expect(Number.isInteger(response.body.error)).to.equal(true);
             expect(response.body.error).to.equal(-1);
             done();
           });
--- server/router/index.js
+++ server/router/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import Index from '../controllers';
 import questionOneSolution from '../controllers/questionOneController';
 import questionTwoSolution from '../controllers/questionTwoController';
+import questionThreeSolution from '../controllers/questionThreeController';
 
 const router = Router();
 
@@ -12,6 +13,9 @@ const routes = (app) => {
   router.post('/api/v1/question-one-solutions/input-validation', questionOneSolution.inputValidation);
   // Object Property Removal Route
   router.patch('/api/v1/question-two-solutions/remove-prop-from-object', questionTwoSolution.removeObjectProperty);
+  // Non existent endpoints
+  // Lowest Index For Aladdin's Circular Trip Route
+  router.post('/api/v1/question-three-solutions/lowest-index-for-circular-trips', questionThreeSolution.lowestCircularTripIndexFinder);
   // Non existent endpoints
   router.use((req, res) => {
     res.status(404).json({
